5d55ce628eaa8911e2b7c9292ba84c6d494e87e1
Quan Dai Tri <qdaitridn2003@gmail.com>
2023-10-20 11:05:30 +0700

    Complete route change password with authentication

--- src/utils/validators/auth.validators.ts
+++ src/utils/validators/auth.validators.ts
@@ -42,3 +42,29 @@ export const passwordValidator = Zod.object({
     path: ['confirmPassword'],
     message: 'Confirm password must be matches to the current password',
 });
+
+export const changePasswordValidator = Zod.object({
+    oldPassword: Zod.string()
+        .min(6, { message: 'Old password must be at least have 6 characters' })
+        .regex(new RegExp('^[a-z0-9]*$'), {
+            message:
+                'Old password must be not have special characters or capital letters',
+        }),
+    newPassword: Zod.string()
+        .min(6, { message: 'New password must be at least have 6 characters' })
+        .regex(new RegExp('^[a-z0-9]*$'), {
+            message:
+                'New password must be not have special characters or capital letters',
+        }),
+    confirmPassword: Zod.string()
+        .min(6, {
+            message: 'Confirm password must be at least have 6 characters',
+        })
+        .regex(new RegExp('^[a-z0-9]*$'), {
+            message:
+                'Confirm password must be not have special characters or capital letters',
+        }),
+}).refine((data) => data.confirmPassword === data.newPassword, {
+    path: ['confirmPassword'],
+    message: 'Confirm password must be matches to the new password',
+});
