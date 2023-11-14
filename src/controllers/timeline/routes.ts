5ba6e076387b82ddcaab21c2f88179f0e43e68ba
Phapdn <106089682+PhapDang3@users.noreply.github.com>
2023-11-14 22:22:57 +0700

    add route timeline

---
+++ src/controllers/timeline/routes.ts
@@ -0,0 +1,23 @@
+import Express from 'express';
+import * as Controller from './controller';
+import { Authorization, CheckRole } from '../../middlewares';
+import { Identify } from '../../constants';
+
+export const timelineRoutes = Express.Router();
+
+timelineRoutes
+    .route('/create-timeline')
+    .post(Authorization, CheckRole([Identify.Admin, Identify.Assistant, Identify.Manager]), Controller.createTimeline);
+
+timelineRoutes
+    .route('/update-timeline/:_id')
+    .put(Authorization, CheckRole([Identify.Admin, Identify.Manager, Identify.Assistant]), Controller.updateTimeLine);
+
+timelineRoutes
+    .route('/delete-timeline/:_id')
+    .delete(
+        Authorization,
+        CheckRole([Identify.Admin, Identify.Assistant, Identify.Manager]),
+        Controller.deleteTimeline,
+    );
+timelineRoutes.route('/get-list-timeline').get(Authorization, Controller.getListTimeline);
